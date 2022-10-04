b74c91184b34b90e760ec4b4a58de3cd984a8ac5
MathiasQM <77293663+MathiasQM@users.noreply.github.com>
2022-10-04 09:42:56 +0200

    new commit

--- serviceworker.js
+++ serviceworker.js
@@ -2,7 +2,7 @@ const cacheName = 'cache-members';
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(cacheName).then(function(cache) {
-      return cache.addAll(['/studentss/', '/studentss/index.html', '/studentss/manifest.json', '/studentss/members.json', '/studentss/serviceworker.js', '/studentss/morten.png', '/studentss/mystyle.css', '/studentss/nina.png', '/studentss/olivia.png']);
+      return cache.addAll(['/studentss/', '/studentss/index.html', '/studentss/members.json', '/studentss/morten.png', '/studentss/mystyle.css', '/studentss/nina.png', '/studentss/olivia.png']);
     })
   );
 });
